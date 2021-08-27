3496ccc8de51352e7b866c2522c11ea7c8368411
Giang Hoang <gianghoang.9955@gmail.com>
2021-08-28 00:20:01 +0700

    update check duplicated with subject

--- factory.py
+++ factory.py
@@ -1,5 +1,6 @@
 from model.model import SBertTokenizer
 from question import Question
+import shutil
 import torch
 import pandas as pd
 import os
@@ -34,14 +35,14 @@ class Factory():
         df = pd.read_csv(source_path)
         data = df['sentence'].values.tolist()
 
-        # self.data[0] += data
-        # encoded = self.encode(self.data[0])
+        self.data[0] += data
+        encoded = self.encode(self.data[0])
 
-        # box = [self.data[0],encoded]
+        box = [self.data[0],encoded]
 
         # overwrite train.pt
-        encoded = self.encode(data)
-        box = [data,encoded]
+        # encoded = self.encode(data)
+        # box = [data,encoded]
 
         torch.save(box,destination_path)
 
@@ -52,6 +53,26 @@ class Factory():
         
         torch.save(box, destination_path)
 
+    def save_encoded_with_subject(self, dataRequest):
+        src = os.path.join(dirname,"train.pt")
+        destination_path =  os.path.join(dirname + "/subject/", dataRequest['subject'] + ".pt")
+        
+        if not os.path.isfile(destination_path):
+            shutil.copyfile(src, destination_path)
+
+        print(destination_path)
+
+        if os.path.isfile(destination_path):
+            with open(destination_path,"rb") as f:
+                data = torch.load(f,map_location=torch.device('cpu'))
+                
+                if len(data[0]) != len(dataRequest['dataBank']):
+                    encoded = self.encode(dataRequest['dataBank'])
+                    box = [dataRequest['dataBank'],encoded]
+                    torch.save(box, destination_path)
+
+
+        
     def load_encoded(self, path=os.path.join(dirname,"train.pt")):
         if os.path.isfile(path):
             with open(path,"rb") as f:
@@ -67,8 +88,8 @@ class Factory():
         else:
             print("model path not exist !!!")
 
-    def find_duplicate(self, input_data: list, confident=0.8):
-        self.load_encoded()
+    def find_duplicate(self, input_data: list, confident=0.8, subject="train"):
+        self.load_encoded(path=os.path.join(dirname + "/subject/", subject + ".pt"))
         if self.data is not None:
             output = self.encode(input_data)
             output = [o.expand(self.data[1][0].size()) for o in output]
--- main.py
+++ main.py
@@ -9,8 +9,9 @@ confident = 0.6
 #===========================================API========================================================================
 
 import uvicorn
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile, Request
 from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
 class Question(BaseModel):
@@ -20,8 +21,9 @@ class FilePath(BaseModel):
     path: str
 
 @app.post("/duplicate/find-duplicate")
-async def create_item(request: Question):
-    fit,response = f.find_duplicate([request.question],confident)
+async def create_item(request: Request):
+    dataRequest = await request.json()
+    fit,response = f.find_duplicate([dataRequest['question']],confident, subject=dataRequest['subject'])
     error = ""
     if response == None:
         return {
@@ -33,13 +35,19 @@ async def create_item(request: Question):
 async def train_data(file_path: FilePath):
     print(file_path.path)
     f.save_encoded(file_path.path)
-    # return "training successful...!"
+    return "training successful...!"
 
 
 @app.post("/duplicate/train-data-sentence")
 async def train_data_sentence(sentences: Question):
     print(sentences)
     f.save_encoded_sentence(sentences)
+    return "training successful...!"
+
+@app.post("/duplicate/train-data-subject")
+async def train_data_subject(request: Request):
+    dataRequest = await request.json()
+    f.save_encoded_with_subject(dataRequest)
     return "training successful...!"
 
 if __name__ == "__main__":
